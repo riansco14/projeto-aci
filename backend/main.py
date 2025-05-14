60b5f3a91ac40a3ab05236452744ce9312bdfc84
Rian <rian.azevedo@qintess.com>
2025-05-13 23:07:03 -0300

    refactor: - inclusao das novas rotas

--- backend/main.py
+++ backend/main.py
@@ -1,36 +1,23 @@
 # main.py
-from typing import Optional, Any, Union
-
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from services.ia.rag_with_fewshot import generate_response_from_prompt
-
-#from services.ia.llama_query import generate_response_from_prompt
+from config.database import Base, engine
+from routes import auth, llm_custom
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # ou ["*"] para liberar geral (menos seguro)
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-class QueryRequest(BaseModel):
-    pergunta: str
 
-class QueryResponse(BaseModel):
-    resposta: Optional[str] = None
-    shape: Optional[Union[int,tuple[int, ...]]] = None
-    colunas: Optional[list[Any]] = None
-    dados: Optional[list[list[Any]]] = None
+# Cria as tabelas no banco
+Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
+app.include_router(llm_custom.router)
 
 
-@app.post("/consultar", response_model=QueryResponse)
-def consultar(request: QueryRequest):
-    try:
-        resultado = generate_response_from_prompt(request.pergunta)
-        return QueryResponse(**resultado)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
